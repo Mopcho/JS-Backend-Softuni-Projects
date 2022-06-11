3c78065f0c52a495e0af1ed5e6b8b6aa2ba3af8b
Mopcho <valeri_petkov01@abv.bg>
2022-06-11 13:45:36 +0300

    Adding endpoints and constants for the app

---
+++ MongoDB and Mongoose/Cubicles with Mongoose/src/configs/constants.js
@@ -0,0 +1,5 @@
+exports.constants = {
+    secret : 'MySecret123456',
+    session : 'session',
+    salt : 10
+}
---
+++ MongoDB and Mongoose/Cubicles with Mongoose/src/configs/endpoints.js
@@ -0,0 +1,13 @@
+exports.endpoints = {
+    cubeCreate : 'cube/create',
+    cubeDelete : 'cube/delete',
+    cubeDetails : 'cube/details',
+    cubeEdit : 'cube/edit',
+    userLogin : 'user/login',
+    userRegister : 'user/register',
+    accessoryAttach : 'accessory/attach',
+    accessoryCreate : 'accessory/create',
+    index : 'index',
+    about : 'about',
+    error : '404'
+}
